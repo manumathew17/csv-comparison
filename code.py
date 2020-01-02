d411ea6475eb383299e50b9624d1c1dd90f03957
manumathew17 <54477058+manumathew17@users.noreply.github.com>
2020-01-02 13:57:09 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,11 +2,11 @@ import csv
 count=0
 starting_point=int(input("enter starting row"))
 ending_point=int(input("enter ending row"))
-with open(r"C:\Users\manumathew\Desktop\fille.csv")as f:
+with open(r"fille.csv")as f:
     data=csv.reader(f)
     for row in  data:
          if int(row[31])>=starting_point and int(row[31])<=ending_point:
-             with open(r"C:\Users\manumathew\Desktop\average.csv") as f3:
+             with open(r"average.csv") as f3:
                 data2=csv.reader(f3)
                 for i in data2:
                      
@@ -46,7 +46,7 @@ with open(r"C:\Users\manumathew\Desktop\fille.csv")as f:
                              p31=i[25]
                              count=count+1
                              print(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p17,p18,p19,p20,p21,p22,p23,p24,p25)
-                             with open(r"C:\Users\manumathew\Desktop\foutput8.csv","a",newline='' ) as f3:
+                             with open(r"output8.csv","a",newline='' ) as f3:
                                  wtr = csv.writer(f3)
                                  wtr.writerows([[p1,p2,p29,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31]])
 print(count)
